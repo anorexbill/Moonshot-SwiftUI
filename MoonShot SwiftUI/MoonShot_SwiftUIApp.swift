5cef7bc74d3c5272097ca0dcdb36e5810255b105
anorexbill <bryteanorchie1@gmail.com>
2021-03-15 04:23:24 -0600

    Initial Commit

---
+++ MoonShot SwiftUI/MoonShot_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShot_SwiftUIApp.swift
+//  MoonShot SwiftUI
+//
+//  Created by Frimpong Anorchie II on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonShot_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
